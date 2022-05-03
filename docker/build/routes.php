9d578d7317610f5deb9eb3fec1a94cd72b4e6288
txroot <andretxrocha@gmail.com>
2022-05-03 16:11:39 +0100

    added php router

---
+++ docker/build/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
+
+// ## Static GET
+get('/', 'index.php');
+
+// ## Dynamic GET using 1 variable
+// The $id will be available in user.php
+get('/circuit/$id', 'circuit.php');
+
+// ## Dynamic GET using 2 variables
+// Both $method and $netlist will be available in academy.php
+get('/academy/$method/$netlist', 'academy.php');
+
+// ## Dynamic GET using 2 variables with static
+// In the URL -> http://domain/circuit/analyse/method/mtn
+// Both $analyse and $method will be available in product.php
+get('/circuit/$analyse/method/:mtn', 'analyse.php');
+
+// ## any method stands for GETs or POSTs
+// The error.php has access to $_GET and $_POST
+any('/error','views/error.php');
