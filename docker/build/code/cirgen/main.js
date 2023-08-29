cf997492f7b58367e0942783dfab58bd37b9c423
1101118 <1101118@isep.ipp.pt>
2023-08-29 20:31:23 +0100

    Função_0.4

--- docker/build/code/cirgen/main.js
+++ docker/build/code/cirgen/main.js
@@ -4,6 +4,7 @@ $(function(){
              circuto: {
                  AC: $("#isAC")[0].checked,
                  DC: $("#isDC")[0].checked,
+                 Nos: $("#isNos")[0].checked,    
                  Malhas: $("#isMalhas")[0].checked,
                  Ramos: $("#isRamos")[0].checked
                   },
@@ -11,13 +12,13 @@ $(function(){
                  Resistência: $("#isResistênca")[0].checked,
                  Condensador: $("#isCondensador")[0].checked,
                  Bobine: $("#isBobine")[0].checked,
-                 Massa: $("#isMassa")[0].checked,
+               
                  Fonte_de_corrente: $("#isFonte de corrente")[0].checked,
                  Fonte_de_tensão: $("#isFonte de tensão")[0].checked 
                   }
                 }
     }
-    function escolherCircuito(){
+    function escolherCircuito(infoutilizador){
         return{
             AC: {
                 Malhas: $("#isMalhas")[0].checked,
