c6b9152235487104ac973f10731babf3966ab0c0
1101118 <1101118@isep.ipp.pt>
2023-10-07 23:15:00 +0100

    Função escolher circuito0.1

--- docker/build/code/cirgen/main.js
+++ docker/build/code/cirgen/main.js
@@ -22,21 +22,64 @@ $(function(){
         return{
             const listaDeNomes = ["NO_R1_M1.json","N2_R3_M2_ESTRELA.json","N2_R3_M2.json","N2_R4_M3.json","N2_R5_M4.json"."N4_R6_M3_TRIANGULO.json"];
             const listaDeCircuitos = listaDeNomes.map(nome =>{
-                ["NO_R1_M1.json","N2_R3_M2_ESTRELA.json","N2_R3_M2.json","N2_R4_M3.json","N2_R5_M4.json"."N4_R6_M3_TRIANGULO.json"];
-                            Malhas: $("#isMalhas")[0].checked,
-                Ramos: $("#isRamos")[0].checked,
-                Nos: $("#isNos")[0].checked           
-            },
-            DC: {
-                Malhas: $("#isMalhas")[0].checked,
-                Ramos: $("#isRamos")[0].checked,
-                Nos: $("#isNos")[0].checked,
+                const partes = nome.match(/N(\d+)_R(\d+)_M(\d+)\.json/);
+                if (partes){
+                    return{
+                        nome_original:nome,
+                        N:parselnt(partes[1]),
+                        R:parselnt(partes[2]),
+                        M:parselnt(partes[3])
+                    };
+                }else{
+                    throw new Error('Nome de arquivo inválido:${nome}');
+                }
+            }),
+            const criterios = {N:[],R:[],M:[]},
+            const circuitosFiltrados=listaDeCircuitos.filter(circuito =>{
+                return(
+                    circuito.N===criterios.N&&
+                    circuito.R===criterios.R&&
+                    circuito.M===criterios.M);
+            }),
+            
+            if(circuitosFiltrados.length===0){
+                throw new Error("Nenhum circuito satisfaz os critérios.");
+            }else if(circuitosFiltrados.length===1){
+                const nomeEscolhido=circuitosFiltrados[0].nome_original;
+                fetch('/lista_nomes_circuitos')
+                .then(responde => responde.json())
+                .then(data =>{
+                    if(data.error){
+                        console.error(data.error);
+                    }else{
+                        const nomesEscolhidos=data.files;
+                        console.log('Circuito escolhido: ${nomeEscolhido}');
+                  }
+                    })
+                    .catch(error=>{
+                        console.error('Erro na solicitação:', error);
+                    });
+                    else{
+                        const indiceAleatorio = Match.floor(Math.random()*circuitosFiltrados.length);
+                        const nomeAleatorio = circuitosFiltrados[indiceAleatorio].nome_original;
+                        fetch('/lista_nomes_circuitos')
+                        .then(responde => responde.json())
+                        .then(data =>{
+                             if(data.error){
+                                console.error(data.error);
+                             }else{
+                                const nomesEscolhidos=data.files;
+                                console.log('Circuito escolhido aleatoriamente: ${nomeAleatorio}');
+                  }
+                    })
+                    .catch(error=>{
+                        console.error('Erro na solicitação:', error);
+                    });
+                }
             }
-
         }
-
     }
-    function preencherCircuito(){
+    function alterarCircuito(){
         return{
             Componentes: {
                 Resistência: $("#isResistênca")[0].checked,
